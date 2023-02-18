12080fa24199050d229f1ac465c22e8b635eeeb1
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-02-18 16:02:49 -0500

    Added main folders

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// This file is for setting express up
+
+import express from 'express';
+import morgan from 'morgan'; // middleware
+import pkg from '../package.json';
+
+const app = express();
+
+app.set('pkg', pkg);
+app.use(morgan('dev'));
+
+// When they ask for the main path
+// I want you to respond with specific info
+app.get('/', (req, res) => {
+    res.json({
+        name: app.get('pkg').name,
+        author: app.get('pkg').author,
+        description: app.get('pkg').description,
+        version: app.get('pkg').version
+    });
+});
+
+export default app;
