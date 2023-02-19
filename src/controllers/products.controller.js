3350d0260f7b72c3144ac01651a843ba2aef0935
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-02-19 18:51:06 -0500

    Added config for creating a product

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,9 +1,11 @@
 // 5 functions that work with the DB
 import Product from "../models/Product";
 
-export const createProduct = (req, res) => {
-    console.log(req.body);
-    res.json('Creating product');
+export const createProduct = async (req, res) => {
+    const { name, category, price, imgURL } = req.body;
+    const newProduct = new Product({ name, category, price, imgURL });
+    const productSaved = await newProduct.save();
+    res.status(201).json(productSaved);
 };
 
 export const getProducts = (req, res) => {
