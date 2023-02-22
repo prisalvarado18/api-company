f2fadf96dcca0e684966d13318b19498bee5d6a3
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-02-21 21:12:54 -0500

    Added password encryption function

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new Schema ({
     username: {
@@ -22,5 +23,25 @@ const userSchema = new Schema ({
     timestamps: true,
     versionKey: false,
 });
+
+// Statics hace referencia a crear metodos estaticos
+// Forma de llamar a un objeto sin necesidad de instanciarlo
+// Este metodo va a recibir una contrasena y la va a cifrar
+userSchema.statics.encryptPassword = async (password) => {
+    // Importo bcrypt
+    // Import genSalt: es una manera de aplicar un algoritmo
+    // y aqui le decimos la cantidad de veces que lo aplicará
+    // A más # de veces, más se ejecuta el algoritmo 
+    // y más recursos consumirá el servidor
+    const salt = await bcrypt.genSalt(10);
+    // password es la contraseña que está tipeando
+    return await bcrypt.hash(password, salt);
+}
+
+// Este metodo recibe dos contrasenas
+// Un password guardado
+// Un password para comparar 
+userSchema.statics.comparePassword = async (password, receivedPassword) => {}
+
 
 export default model('User', userSchema);
